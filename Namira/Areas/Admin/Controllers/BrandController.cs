9d4c9f968d4a3c7b4766aaafeb0500839cf6fb66
Roman-Borysenko <borysenko.roman18@gmail.com>
2020-05-31 14:09:28 +0300

    added the ability to add a brand

---
+++ Namira/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Namira.Areas.Admin.Models;
+using Namira.Data;
+using SlugGenerator;
+
+namespace Namira.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private DataContext context;
+        public BrandController()
+        {
+            context = new DataContext();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Brand brand)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(brand);
+            }
+
+            var mapped = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<Brand, Entities.Brand>()));
+            var entity = mapped.Map<Entities.Brand>(brand);
+
+            entity.Slug = entity.Name.GenerateSlug();
+
+            context.Brands.Add(entity);
+            await context.SaveChangesAsync();
+
+            return RedirectToRoute("admin_area", new { controller = "brand", action = "index" });
+        }
+    }
+}
---
+++ Namira/Areas/Admin/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Namira.Areas.Admin.Models
+{
+    public class Brand
+    {
+        public int? Id { get; set; }
+        [Required, StringLength(120, MinimumLength = 2)]
+        public string Name { get; set; }
+        [StringLength(140, MinimumLength = 1)]
+        public string Slug { get; set; }
+    }
+}
