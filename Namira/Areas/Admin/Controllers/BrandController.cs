2950d45a46a49b67cf330766bf99b98120dbf134
Roman-Borysenko <borysenko.roman18@gmail.com>
2020-05-31 15:52:19 +0300

    added ability to delete and edit brand

--- Namira/Areas/Admin/Controllers/BrandController.cs
+++ Namira/Areas/Admin/Controllers/BrandController.cs
@@ -52,5 +52,32 @@ namespace Namira.Areas.Admin.Controllers
 
             return RedirectToRoute("admin_area", new { controller = "brand", action = "index" });
         }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var mapped = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<Entities.Brand, Brand>()));
+            var brand = mapped.Map<Brand>(await context.Brands.SingleOrDefaultAsync(b => b.Id == id));
+
+            ViewBag.Page = "edit";
+            return View("Add", brand);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Brand brand)
+        {
+            var entity = await context.Brands.SingleOrDefaultAsync(b => b.Id == brand.Id);
+            new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<Brand, Entities.Brand>())).Map<Brand, Entities.Brand>(brand, entity);
+            entity.Slug = entity.Name.GenerateSlug();
+
+            context.Brands.Update(entity);
+            await context.SaveChangesAsync();
+
+            return RedirectToRoute("admin_area", new { controller = "brand", action = "index" });
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            context.Brands.Remove(await context.Brands.SingleOrDefaultAsync(b => b.Id == id));
+            await context.SaveChangesAsync();
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
     }
 }
